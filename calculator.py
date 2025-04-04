585444191bcfd09582727ba41cdae220edd5b411
clarris-c <clarrisngonyo221@gmail.com>
2025-04-04 09:18:40 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def calculator():
+ num1 = 3
+ num2 = 5
+ operation = '+'
+    
+    # Perform the calculation
+    if operation == '+':
+        result = num1 + num2
+        print(f"The result of addition is: {result}")
